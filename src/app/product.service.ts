f9572606063a276a119e61f4108eab4c6bb59db2
rominacongregado <56069518+rominacongregado@users.noreply.github.com>
2020-02-21 13:52:15 +0100

    service check 1

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../ assets / album.json';
+
+  constructor(private _http: Http) { }
+
+
+  getAlbum() {
+    id: Number;
+  }
+
+}
